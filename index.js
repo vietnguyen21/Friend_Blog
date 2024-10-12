989d1307f342991cb260316da5233edf529dff4e
Nguyen Tat Hoang Viet <94428612+vietnguyen21@users.noreply.github.com>
2024-10-12 23:56:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -181,6 +181,7 @@ app.get('/post/:id', (req, res) => {
     });
 });
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
